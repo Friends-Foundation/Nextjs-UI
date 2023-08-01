66eb84b14272d59b3172b389bd69f8881a57130a
vishalvx <vprajapati@horizontal.com>
2023-08-01 14:07:58 +0530

    first commit

---
+++ .storybook/main.ts
@@ -0,0 +1,33 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
+
+const config: StorybookConfig = {
+  stories: [
+    "../components/**/*.mdx",
+    "../components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  // This Code is needed for the @import Alias
+  webpackFinal: async (config) => {
+    if (config.resolve?.alias) {
+      config.resolve.alias = {
+        ...config.resolve?.alias,
+        "@": path.resolve(__dirname, "../"), //add path according to your story location ie. shared folder ../shared
+      };
+    }
+    return config;
+  },
+};
+export default config;
