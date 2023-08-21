f81517eff11d14628305a026aa1627e272d59c81
Ravi_30 <rvadher@horizontal.com>
2023-08-21 12:42:43 +0530

    Added new Component -Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,38 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/Headers/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      screens: {
+        sx: "320px",
+        sm: "600px",
+        md: "768px",
+        lg: "1024",
+        xl: "1200px",
+        "2xl": "1224px",
+      },
+      colors: {
+        "gray-1": "#dfe7ea",
+        "gray-2":"#f4f7f8",
+        link: "#67757c",
+        purple:"#154998;"
+      },
+      transitionProperty: {
+        height: "height",
+        spacing: "margin, padding",
       },
     },
   },
   plugins: [],
-}
+};
